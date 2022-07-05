68281929e398032c79325f8e0f9201d73db02767
Utkatsh Kaule <utkarshkaule7@gmail.com>
2022-07-05 21:24:40 +0530

    Create nearstSmallerElement_Right.js

---
+++ nearstSmallerElement_Right.js
@@ -0,0 +1,33 @@
+// Nearest Small Element To Left
+let arr = [4, 5, 2, 10, 8];
+let s = [];
+let result = [];
+
+
+
+for (let i = arr.length - 1; i > 0; i--) {
+    if (s.length == 0) {
+        result.push(-1);
+    }
+    else if (s.length > 0 && s[s.length - 1] < arr[i]) {
+        result.push(s[s.length - 1]);
+    }
+    if (s.length > 0 && s[s.length -1] >= arr[i]) {
+        while (s.length > 0 && s[s.length - 1] >= arr[i]) {
+            s.pop()
+        }
+        if (s.length == 0) {
+            result.push(-1);
+        }
+        else {
+            result.push(s[s.length - 1])
+        }
+    }
+    s.push(arr[i])
+}
+let final = []
+for(let i = result.length -1 ; i >= 0 ; i--){
+    final.push(result[i]);
+}
+
+console.log(final);
