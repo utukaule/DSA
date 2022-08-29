df20db25d75d973406234b0a39d76b4ecd0f0539
Utkatsh Kaule <utkarshkaule7@gmail.com>
2022-08-29 22:05:14 +0530

    LB DSA

---
+++ LB DSA/1st and 2nd lecture/introToCpp.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // cout << "Namaste duniya" << endl;
+    // int a = 123;
+    // cout<< a << endl;
+
+    // char ch = 'u';
+    // cout<<ch << endl;
+
+    // bool bl = true;
+    // cout<<bl <<endl;
+
+    // float x = 1.2;
+    // cout <<x <<endl;
+
+    // int size = sizeof(a);
+    // cout << "size of a is : "<<size <<endl;
+    // int z = -12;
+    // cout <<z;
+
+    //unsigned for only +ve number from 0 to 2^32
+    // unsigned int z = 123;
+    // cout << z;
+    // double a = 2.0/5;
+    // cout<<a;
+
+    // int a = 2;
+    // int b = 3;
+    // bool first = (a<b);
+    // cout << first<<endl;
+
+    char a = 97;
+    cout<< a<< endl;
+    
+}
---
+++ LB DSA/3rd lecture/loops&patterns.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//conditions and loops OR decision making box
+
+//progrem 1
+
+/*int a = 2;
+int b = 3 ;
+if(a<b){
+    cout<<"b is greater then a";
+}
+else{
+    cout<<"a is greater then b";
+}
+*/
+
+
+//program 2
+
+/*
+  int a, b;
+  cout << "Enter the value of a "<<endl;
+  cin >> a ;
+  cout << "Enter the value of b"<<endl;
+  cin >> b ;
+    if (a < b) { 
+    cout << "b is greater then a"; 
+    }
+    else if(a == b){
+        cout<<"Both are equal";
+    }
+  else {
+    cout << "a is greater then b";
+  }
+*/
+
+
+//program 3 
+// check number is negative or not
+
+/*
+int a;
+cout<<"Enter the value of a"<<endl;
+cin>> a;
+if(a > 0 ){
+    cout<<"a is +";
+}
+else{
+    cout<< " a is -";
+}
+*/
+
+
+
+//Problem 4 
+// check the input is capital latter or small latter or number
+
+/*
+char ch;
+
+ch = cin.get();
+
+if(ch >=16 && ch <= 26){
+    cout<<"Number";
+}
+
+else if(ch >= 65 && ch <= 90){
+    cout<<"Capital latter";
+}
+else{
+    cout<<"Small latter";
+}
+*/
+
+
+
+
+
+}
