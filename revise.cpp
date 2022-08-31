f2a87e15b9973e4f7785abd56bcc982087e8f375
Utkatsh Kaule <utkarshkaule7@gmail.com>
2022-08-31 23:23:20 +0530

    5th lecture

---
+++ revise.cpp
@@ -0,0 +1,17 @@
+    // Problem 23
+    //    1
+    //   121
+    //  12321
+    // 1234321
+
+    #include<iostream>
+    using namespace std;
+
+    int main(){
+        int n;
+        cin>>n;
+        int row= 1;
+        while(row<= n){
+            int space = 
+        }
+    }
