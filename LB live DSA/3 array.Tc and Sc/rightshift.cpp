19dea1a939a412030fad6fe1e278245b8bf0f4d4
Utkatsh Kaule <utkarshkaule7@gmail.com>
2023-09-13 18:21:31 +0530

    array questions solved
    array questions solved

---
+++ LB live DSA/3 array.Tc and Sc/rightshift.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void shiftLeft(int arr[], int n){
+    int temp = arr[n-1];
+    for(int i = n-1 ; i >=1 ; i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0] = temp;
+}
+
+int main(){
+    int arr[] = {10,20,30,40,50};
+    int n = 5;
+     shiftLeft(arr,n);
+    for(int i = 0; i < n ; i++){
+        cout<< arr[i]<<" ";
+    }
+}
